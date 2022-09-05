11df686909ac7ff1d3970ed5f876d6b00f7e9a4f
aw22433 <101360136+aw22433@users.noreply.github.com>
2022-09-05 21:37:21 +0100

    Add files via upload

---
+++ Unit 9 - Portfolio Activity  - Testing with Assert.py
@@ -0,0 +1,15 @@
+
+# Python String Operations
+str1 = 'Hello'
+str2 ='World!'
+assert type(str1)==str,"Error with str1"
+assert type(str2)==str,"Error with str2"
+
+
+# using +
+print('str1 + str2 = ', str1 + str2)
+assert type(str1+str2) == str, "Error with str1+str2"
+
+# using *
+print('str1 * 3 =', str1 * 3)
+assert type(str1*3)== str, "Error with str1*3"
